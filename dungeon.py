cd5701042a7aa081c270d118cf0d0381b2bd3a0d
Gary Lam <garykailam@gmail.com>
2020-12-04 21:26:20 -0800

    drawer places doorways, rows and columns mixed up

--- dungeon.py
+++ dungeon.py
@@ -4,10 +4,16 @@ from dungeondraw import MapDisplay
 class RoomNode:
     def __init__(self, row, column):
         self.position = (row, column)
-        self.doors = {"north": None,
-                      "south": None,
-                      "east": None,
-                      "west": None}
+        self.doors = {"north": random.randint(0, 4),
+                      "south": random.randint(0, 4),
+                      "east": random.randint(0, 4),
+                      "west": random.randint(0, 4)
+                      }
+        # self.doors = {"north": 0,
+        #               "south": 0,
+        #               "east": 0,
+        #               "west": 0}
+
         self.info = None
         self.unique = {"Abstraction": False,
                       "Encapsulation": False,
@@ -26,7 +32,7 @@ class Dungeon:
     def generate(self, rows, columns):
         if rows * columns < 6:
             raise ValueError("Dungeon size is too small.")
-        elif rows * columns > 144:
+        elif rows * columns > 225:
             raise ValueError("Dungeon size is too large.")
         self.rows = rows
         self.cols = columns
@@ -37,12 +43,12 @@ class Dungeon:
 
     def set_borders(self):
         for RoomNode in self.grid[0]:
-            RoomNode.doors["north"] = False
+            RoomNode.doors["north"] = 0
         for RoomNode in self.grid[self.rows-1]:
-            RoomNode.doors["south"] = False
+            RoomNode.doors["south"] = 0
         for row in self.grid:
-            row[0].doors["west"] = False
-            row[self.cols-1].doors["east"] = False
+            row[0].doors["west"] = 0
+            row[self.cols-1].doors["east"] = 0
 
     def set_walls(self):
         pass
@@ -63,8 +69,8 @@ class Dungeon:
                     unique[key] = self.grid[randrow][randcol]
                     self.grid[randrow][randcol].unique[key] = True
         #test lines
-        for key, value in unique.items():
-            print(f'{key} at {value}')
+        # for key, value in unique.items():
+        #     print(f'{key} at {value}')
 
     def traverse(self):
         for list in self.grid:
@@ -74,15 +80,24 @@ class Dungeon:
     def draw(self):
         display = MapDisplay(self.cols, self.rows)
         display.draw_border()
-        display.draw_room()
+        for list in self.grid:
+            for room in list:
+                display.draw_room(room.position[0], room.position[1], room.doors)
         display.root.mainloop()
 
 
 if __name__ == '__main__':
 
     test = Dungeon()
-    test.generate(5,5
-    test.draw
+    test.generate(6,9)
+    test.draw()
+    print(test.grid[1][4])
+    test.grid[1][4].doors["north"] = 3
+    test.grid[1][4].doors["south"] = 3
+    test.grid[1][4].doors["east"] = 3
+    test.grid[1][4].doors["west"] = 3
+    print(test.grid[1][4])
+    test.draw()
 
 
 
--- dungeondraw.py
+++ dungeondraw.py
@@ -7,20 +7,18 @@ class MapDisplay:
         self.columns  = columns
         self.room_unit = 75
         self.root = Tk()
-        self.canvas = Canvas(self.root, height=rows*self.room_unit, width=columns*self.room_unit)
-        self.canvas.pack(fill="both")
+        self.canvas = Canvas(self.root, height=rows*self.room_unit, width=columns*self.room_unit, bg="white")
+        self.canvas.pack()
 
     def draw_border(self):
-        self.canvas.create_rectangle(0, 0, self.rows*self.room_unit,self.columns*self.room_unit, fill="white")
-        for i in range(1, self.rows - 1):
-            self.canvas.create_line(0,i*self.room_unit, self.columns*self.room_unit, i*self.room_unit)
-        for i in range(1, self.columns - 1):
-            self.canvas.create_line(i * self.room_unit, 0, i * self.room_unit, self.rows * self.room_unit)
+        for i in range(1, self.rows):
+            self.canvas.create_line(0,i*self.room_unit, self.columns*self.room_unit, i*self.room_unit, width=4)
+        for i in range(1, self.columns):
+            self.canvas.create_line(i * self.room_unit, 0, i * self.room_unit, self.rows * self.room_unit, width=4)
 
     def draw_room(self, x, y, doors):
-        self.canvas.create_rectangle(self.room_unit*x, self.room_unit*y, self.room_unit*(x+1), self.room_unit*(y+1))
-        for key in doors:
-            if key:
+        for key, value in doors.items():
+            if value > 2:
                 self.draw_door(key, x, y)
 
         # if room is unique_room:
@@ -35,5 +33,16 @@ class MapDisplay:
 
     def draw_door(self, key, x, y):
         if key == "north":
-            self.canvas.create_rectangle(
-        elif key ==
+            self.canvas.create_rectangle(self.room_unit * x + 10, self.room_unit * y - 2, self.room_unit * x + 65,
+                                         self.room_unit * y + 2, fill="white", outline="")
+        elif key == "south":
+            y+=1
+            self.canvas.create_rectangle(self.room_unit * x + 10, self.room_unit * y - 2, self.room_unit * x + 65,
+                                         self.room_unit * y + 2, fill="white", outline="")
+        elif key == "west":
+            self.canvas.create_rectangle(self.room_unit * x - 2, self.room_unit * y + 10, self.room_unit * x + 2,
+                                         self.room_unit * y + 65, fill="white", outline="")
+        elif key == "east":
+            x+=1
+            self.canvas.create_rectangle(self.room_unit * x + 2, self.room_unit * y + 10, self.room_unit * x - 2,
+                                         self.room_unit * y + 65, fill="white", outline="")
