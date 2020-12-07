f60e9be55bfdfafb39b1929551aedb537b2971ae
Gary Lam <garykailam@gmail.com>
2020-12-06 21:32:21 -0800

    places pillars, exit, entrance in random location

--- dungeon.py
+++ dungeon.py
@@ -9,37 +9,34 @@ class RoomNode:
                       "east": random.randint(0, 4),
                       "west": random.randint(0, 4)
                       }
-        # self.doors = {"north": 0,
-        #               "south": 0,
-        #               "east": 0,
-        #               "west": 0}
 
         self.info = None
-        self.unique = {"Abstraction": False,
-                      "Encapsulation": False,
-                      "Inheritance": False,
-                      "Polymorphism": False,
-                      "Entrance": False,
-                      "Exit": False}
+        self.pillar = None
+        self.isexit = False
+        self.isentrance = False
 
     def __str__(self):
         return f'{self.position}, {self.doors}'
 
 class Dungeon:
-    def __init__(self):
-        pass
-
-    def generate(self, rows, columns):
+    def __init__(self, rows, columns):
         if rows * columns < 6:
             raise ValueError("Dungeon size is too small.")
         elif rows * columns > 225:
             raise ValueError("Dungeon size is too large.")
         self.rows = rows
         self.cols = columns
-        self.grid = [[RoomNode(i,j) for j in range(columns)] for i in range(rows)]
+        self.unique_rooms = []
+
+    def generate(self):
+        self.grid = [[RoomNode(r,c) for c in range(self.cols)] for r in range(self.rows)]
         self.set_borders()
-        self.set_walls()
-        self.set_unique_rooms()
+        self.set_pillars()
+        self.set_entrance_and_exit()
+        completable = self.check_traversal()
+        while completable == False:
+            self.generate()
+            completable = self.check_traversal()
 
     def set_borders(self):
         for RoomNode in self.grid[0]:
@@ -50,54 +47,54 @@ class Dungeon:
             row[0].doors["west"] = 0
             row[self.cols-1].doors["east"] = 0
 
-    def set_walls(self):
-        pass
+    def set_pillars(self):
+        pillars = ("Abstraction", "Encapslation", "Inheritance", "Polymorphism")
 
-    def set_unique_rooms(self):
-        unique = {"Abstraction": None,
-                  "Encapsulation": None,
-                  "Inheritance": None,
-                  "Polymorphism": None,
-                  "Entrance": None,
-                  "Exit": None}
-
-        for key in unique:
-            while unique[key] is None:
-                randrow = random.randint(0, self.rows - 1)
-                randcol = random.randint(0, self.cols-1)
-                if self.grid[randrow][randcol] not in unique.values():
-                    unique[key] = self.grid[randrow][randcol]
-                    self.grid[randrow][randcol].unique[key] = True
+        for key in pillars:
+            unique_room = self.find_rand_room()
+            self.unique_rooms.append(unique_room)
+            unique_room.pillar = key
         #test lines
-        # for key, value in unique.items():
+        # for key, value in pillars.items():
         #     print(f'{key} at {value}')
 
-    def traverse(self):
-        for list in self.grid:
-            for RoomNode in list:
-                yield RoomNode
+    def find_rand_room(self):
+        randrow = random.randint(0, self.rows - 1)
+        randcol = random.randint(0, self.cols - 1)
+        if self.grid[randrow][randcol] not in self.unique_rooms:
+            return self.grid[randrow][randcol]
+        else:
+            return self.find_rand_room()
+
+
+    def set_entrance_and_exit(self):
+        entrance = self.find_rand_room()
+        entrance.isentrance = True
+        self.unique_rooms.append(entrance)
+        exit = self.find_rand_room()
+        exit.isexit = True
+        self.unique_rooms.append(exit)
+        print(f'Entrance at {entrance} \nExit at {exit}')
+
+
+    def check_traversal(self):
+        pass
+
 
     def draw(self):
-        display = MapDisplay(self.cols, self.rows)
+        display = MapDisplay(self.rows, self.cols)
         display.draw_border()
         for list in self.grid:
             for room in list:
-                display.draw_room(room.position[0], room.position[1], room.doors)
+                display.draw_room(room)
         display.root.mainloop()
+
 
 
 if __name__ == '__main__':
 
-    test = Dungeon()
-    test.generate(6,9)
+    test = Dungeon(3,3)
+    test.generate()
     test.draw()
-    print(test.grid[1][4])
-    test.grid[1][4].doors["north"] = 3
-    test.grid[1][4].doors["south"] = 3
-    test.grid[1][4].doors["east"] = 3
-    test.grid[1][4].doors["west"] = 3
-    print(test.grid[1][4])
-    test.draw()
-
 
 
