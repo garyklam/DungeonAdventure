d6e879726887d6dfab140fbe4cf75c66ae4691d4
Gary Lam <garykailam@gmail.com>
2020-12-15 14:07:31 -0800

    finished movement, room check, potion use, and game end cases

--- main_game.py
+++ main_game.py
@@ -8,14 +8,18 @@ class Main_Game:
         self.dungeon = dungeon
         self.adventurer = adventurer
         self.location = self.adventurer.current_location
-
         self._menu_init()
         self._interface_init()
         self._display_init()
+        self.locate_position()
 
     @property
     def frame(self):
         return self._frame
+
+    def locate_position(self):
+        self.location = self.adventurer.current_location
+        self.drawer.set_position(self.location)
 
     def _menu_init(self):
         def confirm_exit(root):
@@ -63,67 +67,140 @@ class Main_Game:
     def _interface_init(self):
         self.text_display = scrolledtext.ScrolledText(self.frame, height=8, font="Times 12", wrap="word")
         self.text_display.grid(row=1, column=0, rowspan=3)
-        self.text_display.insert("1.0", "Welcome to the 502 Dungeon.\nFind all four pillars to unlock the exit.")
-        self.text_display.configure(state="disabled")
-        north = Button(self.frame, text="North", command=lambda:self._move_adventurer("north"), pady=5)
-        north.grid(row=1, column=2, columnspan=2)
-        south = Button(self.frame, text="South", command=lambda:self._move_adventurer("south"), pady=5)
-        south.grid(row=3, column=2, columnspan=2)
-        east = Button(self.frame, text="East", command=lambda:self._move_adventurer("east"), pady=5)
-        east.grid(row=2, column=4)
-        west = Button(self.frame, text="West", command=lambda:self._move_adventurer("west"), pady=5)
-        west.grid(row=2, column=1)
-        self._set_move_button_state(north, south, east, west)
+        intro_text = "Welcome to the 502 Dungeon.\nFind all four pillars to unlock the exit.\n"
+        self.add_text(intro_text)
+        self.north = Button(self.frame, text="North", command=lambda:self._move_adventurer("north"), pady=5)
+        self.north.grid(row=1, column=2, columnspan=2)
+        self.south = Button(self.frame, text="South", command=lambda:self._move_adventurer("south"), pady=5)
+        self.south.grid(row=3, column=2, columnspan=2)
+        self.east = Button(self.frame, text="East", command=lambda:self._move_adventurer("east"), pady=5)
+        self.east.grid(row=2, column=4)
+        self.west = Button(self.frame, text="West", command=lambda:self._move_adventurer("west"), pady=5)
+        self.west.grid(row=2, column=1)
+        self._set_move_button_state()
         player_info = Button(self.frame, text="Profile", command=self._show_adventurer_info)
         player_info.grid(row=2, column=2, columnspan=2)
-        use_health = Button(self.frame, text="Use Health Potion")
+        use_health = Button(self.frame, text="Use Health Potion", command=self.use_healing_pot)
         use_health.grid(row=4, column=1, columnspan=2)
-        use_vision = Button(self.frame, text="Use Vision Potion")
+        use_vision = Button(self.frame, text="Use Vision Potion", command=self.use_vision_pot)
         use_vision.grid(row=4, column=3, columnspan=2)
-        
+
+    def use_healing_pot(self):
+        try:
+            healing_text = f'You use a healing potion and regain {self.adventurer.healing_potion[0]} health points.\n'
+            self.add_text(healing_text)
+            self.adventurer.use_healing_potion()
+        except IndexError:
+            no_potion = "You have no healing potions to use!\n"
+            self.add_text(no_potion)
+
+    def use_vision_pot(self):
+        if self.adventurer.vision_potion > 0:
+            vision_text = "You use a vision potion and gain knowledge of the adjacent rooms.\n"
+            self.add_text(vision_text)
+            self.adventurer.use_vision_potion()
+            row = self.location[0]
+            col = self.location[1]
+            adjacent_rooms = []
+            for i in range(-1, 2):
+                for j in range(-1, 2):
+                    if self.dungeon.in_bounds(row+i, col+j):
+                        adjacent_rooms.append(self.dungeon.get_room(row+i, col+j))
+            print(adjacent_rooms)
+            for room in adjacent_rooms:
+                if room not in self.dungeon.visited_rooms:
+                    self.dungeon.visited_rooms.append(room)
+            self.drawer.draw()
+        else:
+            no_potion = "You have no vision potions to use!\n"
+            self.add_text(no_potion)
+
     def _move_adventurer(self, direction):
         if direction == "north":
-            pass
+            self.adventurer.set_location(self.location[0]-1, self.location[1])
         elif direction =="south":
-            pass
+            self.adventurer.set_location(self.location[0]+1, self.location[1])
         elif direction == "east":
-            pass
+            self.adventurer.set_location(self.location[0], self.location[1]+1)
         elif direction == "west":
-            pass
+            self.adventurer.set_location(self.location[0], self.location[1]-1)
+        self.locate_position()
+        if self.dungeon.get_room(self.location[0], self.location[1]) not in self.dungeon.visited_rooms:
+            text = "\nYou cautiously explore a new room."
+            self.add_text(text)
+        self.check_room()
+        self.drawer.draw()
+        self._set_move_button_state()
 
-    def _set_move_button_state(self, north, south, east, west):
+    def check_room(self):
+        self.add_text("\n")
+        room = self.dungeon.get_room(self.location[0], self.location[1])
+        self.dungeon.visited_rooms.append(room)
+        if room.pit():
+            self.adventurer.decrease_hit_points(room.damage_points())
+            damage_text = f'You take {room.damage_points()} of damage from falling into a pit! ' \
+                          f'You have {self.adventurer.hit_point()}hp left.\n'
+            self.add_text(damage_text)
+            if self.adventurer.hit_point() <= 0:
+                self.game_over("lost")
+        if room.healing_potion():
+            potion = room.get_Potion_HitPoint()
+            self.adventurer.take_healing_potion(potion)
+            room.take_healing_potion()
+            healing_pot_text = "You find a healing potion and quickly take it.\n"
+            self.add_text(healing_pot_text)
+        if room.vision_potion():
+            self.adventurer.take_vision_potion()
+            room.take_vision_potion()
+            vision_pot_text = "You find a vision potion, this will come in handy!\n"
+            self.add_text(vision_pot_text)
+        if room.pillar():
+            pillar = room.pillar()
+            self.adventurer.take_pillar(pillar)
+            room.take_pillar()
+            pillar_text = f'You find the physical embodiment of the concept of {pillar}. When you touch the pillar,' \
+                          f' it disappears and you feel slighly better at programming.\n'
+            self.add_text(pillar_text)
+        if room.exit():
+            if self.adventurer.has_all_pillars():
+                self.game_over("win")
+            else:
+                more_pillars_text = "You do not have all the pillars needed to open the exit, keep on looking!"
+                self.add_text(more_pillars_text)
+
+    def _set_move_button_state(self):
         row, col = self.location[0], self.location[1]
         if self.dungeon.check_north(row, col):
-            north["state"] = "normal"
+            self.north["state"] = "normal"
         else:
-            north["state"] = "disabled"
+            self.north["state"] = "disabled"
         if self.dungeon.check_south(row, col):
-            south["state"] = "normal"
+            self.south["state"] = "normal"
         else:
-            south["state"] = "disabled"
+            self.south["state"] = "disabled"
         if self.dungeon.check_east(row, col):
-            east["state"] = "normal"
+            self.east["state"] = "normal"
         else:
-            east["state"] = "disabled"
+            self.east["state"] = "disabled"
         if self.dungeon.check_west(row, col):
-            west["state"] = "normal"
+            self.west["state"] = "normal"
         else:
-            west["state"] = "disabled"
+            self.west["state"] = "disabled"
 
     def _show_adventurer_info(self):
         player_info = Toplevel()
         player_info.title(self.adventurer.name)
         hp = Label(player_info, text="Health:", font="Times 16").grid(row=0, column=0, rowspan=2)
         total_hp_bar = Label(player_info, bg="white", padx=100).grid(row=1, column=1)
-        actual_hp_bar = Label(player_info, bg="red", padx=self.adventurer.hit_point).grid(row=1, column=1)
-        hp_text = Label(player_info, text=f'{self.adventurer.hit_point}/100', font="Times 12").grid(row=0, column=1)
+        actual_hp_bar = Label(player_info, bg="red", padx=self.adventurer.hit_point()).grid(row=1, column=1)
+        hp_text = Label(player_info, text=f'{self.adventurer.hit_point()}/100', font="Times 12").grid(row=0, column=1)
         potions = Label(player_info, text="Potions:", font="Times 16").grid(row=2, column=0)
         potion_text = f'{self.adventurer.vision_potion} Vision Potion(s)\n'
         if len(self.adventurer.healing_potion) == 0:
             potion_text += "0 Health Potions"
         else:
             for potion in self.adventurer.healing_potion:
-                potion_text += f'Health Potion({potion})\n'
+                potion_text += f'Health Potion({potion}hp)\n'
         potion_list = Label(player_info, text=potion_text, font="Times 14").grid(row=3, column=1)
         pillars = Label(player_info, text="Pillars:", font="Times 16").grid(row=4, column=0)
         pillar_text = ""
@@ -133,7 +210,35 @@ class Main_Game:
 
     def _display_init(self):
         self.display = Canvas(self.frame, height=620, width=620, bg="white")
-        drawer = GameDisplay(self.display, self.dungeon, self.adventurer)
-        drawer.draw()
+        self.drawer = GameDisplay(self.display, self.dungeon)
+        self.drawer.draw()
         self.display.grid(row=0, column=0, columnspan=4)
 
+    def add_text(self, string):
+        self.text_display.configure(state="normal")
+        self.text_display.insert("insert", string)
+        self.text_display.configure(state="disabled")
+
+    def game_over(self, condition):
+        def close_game(root):
+            root.destroy()
+        def replay_game(current):
+            end.destroy()
+            current.destroy()
+        end = Toplevel()
+        replay = Button(end, text="Replay", font="Times 16", command=lambda:replay_game(self.frame))
+        replay.grid(row=2, column=1)
+        exit = Button(end, text="Exit", font="Times 16", command=lambda:close_game(self.root))
+        exit.grid(row=2, column=2)
+        if condition == "lost":
+            end.title("GAME OVER")
+            text = Label(end, font="Times 20",
+                         text="You've lost all of your health and lose conciousness.\nBetter luck next time.")
+        else:
+            end.title("CONGRATULATIONS")
+            text = Label(end, font="Times 20",
+                         text="You've sucessfully escaped the dungeon!\nFeel free to try again on a higher difficulty.")
+            drawer = MapDisplay(self.dungeon, end)
+            entire_map = drawer.draw_entire_map()
+            entire_map.grid(row=0, column=1, columnspan=2)
+        text.grid(row=1, column=0, columnspan=4)
